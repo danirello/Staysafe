2579773624bda757e1439f1c5ff6ff5f1926ee21
SaulGvrs <saulgvrs@gmail.com>
2022-04-26 20:25:08 -0500

    Creando el inicio de sesión v2

--- app/src/main/java/com/example/staysafe/login.java
+++ app/src/main/java/com/example/staysafe/login.java
@@ -8,6 +8,12 @@ public class login extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        try {
+            Thread.sleep(2000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        setTheme(R.style.Theme_Staysafe);
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
     }
