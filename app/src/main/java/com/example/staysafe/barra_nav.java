562694db8f12814b30e3e044bfd6abd0186f4c1d
danirello <danirello888@gmail.com>
2022-05-03 11:30:34 -0500

    anyway

--- app/src/main/java/com/example/staysafe/barra_nav.java
+++ app/src/main/java/com/example/staysafe/barra_nav.java
@@ -1,14 +1,54 @@
 package com.example.staysafe;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
+import android.view.MenuItem;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class barra_nav extends AppCompatActivity {
 
+     principal Principal = new principal();
+     configuraciones Configuraciones = new configuraciones();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_barra_nav);
+
+        BottomNavigationView navigation = findViewById(R.id.bottom_navigation);
+        navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+
+        loadFragment(Principal);
+
     }
+
+    private final BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener = new BottomNavigationView.OnNavigationItemSelectedListener() {
+        @Override
+        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+
+        switch (item.getItemId()){
+            case R.id.Principal:
+                loadFragment(Principal);
+                return true;
+            case R.id.Configuraciones:
+                loadFragment(Configuraciones);
+                return true;
+        }
+            return false;
+        }
+
+
+};
+
+
+public void loadFragment(Fragment fragment) {
+    FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+    transaction.replace(R.id.frame_container, fragment);
+    transaction.commit();
+
+}
 }
