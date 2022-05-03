008199dbbd5f9abc074a352b2c2ebfb8ccc75057
danirello <danirello888@gmail.com>
2022-05-02 19:44:58 -0500

    anyway

---
+++ app/src/main/java/com/example/staysafe/barra_nav.java
@@ -0,0 +1,14 @@
+package com.example.staysafe;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class barra_nav extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_barra_nav);
+    }
+}
